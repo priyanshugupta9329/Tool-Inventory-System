63a46ce6142d39a4f8bbc723b84a737f676b1796
sridevi153 <82940606+sridevi153@users.noreply.github.com>
2023-03-14 10:57:41 +0530

    Delete UserRepository.java

--- src/main/java/in/thoughtclan/ToolInventory/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package in.thoughtclan.ToolInventory.repositories;
-
-import in.thoughtclan.ToolInventory.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-
-//    boolean findAllById(int id);
-}
